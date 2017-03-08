ff8d83c21645480cadd75132744c46adad792b2d
Robert Burger <robert.burger@dlr.de>
2017-03-08 09:05:37 +0100

    removed dependency links_and_nodes

--- src/main/cpp/cli_bridge.cpp
+++ src/main/cpp/cli_bridge.cpp
@@ -43,10 +43,10 @@ namespace cli_bridge {
 
     Client::Client(const char*& bridgename, YAML::Node& node)
 		: bridge_base(bridgename, "bridge_cli", node), cliServer(this, getPort()) {
-        robotkernel::service_provider_t *sp = new robotkernel::service_provider_t();
+        robotkernel::bridge::cbs_t *sp = new robotkernel::bridge::cbs_t();
         sp->add_service = std::bind(&cli_bridge::Client::addService, this, _1);
         sp->remove_service = std::bind(&cli_bridge::Client::removeService, this, _1);
-        robotkernel::kernel::get_instance()->add_service_provider(sp);
+        robotkernel::kernel::get_instance()->add_bridge_cbs(sp);
 
         cliServer.onConnectHandler = std::bind(&cli_bridge::Client::onCliConnect, this, _1);
         cliServer.onDisconnectHandler = std::bind(&cli_bridge::Client::onCliDisconnect, this, _1);
