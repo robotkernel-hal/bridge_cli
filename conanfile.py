ab6cad015294a34d4c8ea2a0556cf18e6eea4f13
Robert Burger <robert.burger@dlr.de>
2025-07-29 16:14:08 +0200

    change: service_helper and robotkernel unstable

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class MainProject(ConanFile):
     name = "bridge_cli"
     description = "robotkernel service bridge command line interface"
     exports_sources = ["*", "!.gitignore"]
-    requires = "robotkernel/6.0.0-vec-rework@robotkernel/unstable"
+    requires = "robotkernel/6.0.0@robotkernel/unstable"
 
     def source(self):
         self.run(f"sed 's/AC_INIT(.*/AC_INIT([bridge_cli], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
