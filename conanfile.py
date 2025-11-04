f89c0f32b26880194a7d40b8b1d4f6209e9794ec
Robert Burger <robert.burger@dlr.de>
2025-11-04 11:18:35 +0100

    fix: deps package versions

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,13 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~=5]@robotkernel/stable"
+    python_requires = "conan_template/[~5]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "bridge_cli"
     description = "robotkernel service bridge command line interface"
     exports_sources = ["*", "!.gitignore"]
-    requires = "robotkernel/6.0.0@robotkernel/unstable"
+    requires = "robotkernel/[~6]@robotkernel/unstable"
 
     def source(self):
         self.run(f"sed 's/AC_INIT(.*/AC_INIT([bridge_cli], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
