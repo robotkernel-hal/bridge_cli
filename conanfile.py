f202e6330f96130a3d8992b70a772a1c7365b722
Robert Burger <robert.burger@dlr.de>
2026-05-19 09:31:09 +0200

    fix: using stable deps

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,11 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~6]@robotkernel/unstable"
+    python_requires = "conan_template/[~6]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "bridge_cli"
     description = "robotkernel service bridge command line interface"
     exports_sources = ["*", "!.gitignore"]
-    requires = "robotkernel/[~6]@robotkernel/unstable"
+    requires = "robotkernel/[~6]@robotkernel/stable"
 
